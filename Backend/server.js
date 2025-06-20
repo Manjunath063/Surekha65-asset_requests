9c88c227c0f9f5c858c4dec571ebc73932748db9
Manjunath063 <manjunathjangam198@gmail.com>
2025-06-20 15:06:50 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -169,5 +169,5 @@ app.delete('/api/requests/clear', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://16.16.64.41:${port}`);
+    console.log(`Server running at http://51.21.244.195:${port}`);
 });
